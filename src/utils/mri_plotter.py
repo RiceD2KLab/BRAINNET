8a9691df40ce9f38c1b1b37cb82b240afe28c13c
Keith Pulmano <kp52@rice.edu>
2023-05-26 05:46:58 -0500

    added mri plotter

--- src/utils/mri_plotter.py
+++ src/utils/mri_plotter.py
@@ -21,7 +21,7 @@ class MRIPlotter:
             if slice_idx is None:
                 slice_idx = self.get_largest_tumor_slice_idx(img_data)[0]
             
-        display_data = img_data[:, :, slice_idx]
+            display_data = img_data[:, :, slice_idx]
 
         # axs can be 1d or 2d
         axs_element = axs
@@ -32,18 +32,26 @@ class MRIPlotter:
         
         if title:
             axs_element.set_title(title)
-        
+            
         if auto_cbar:
             fig.colorbar(img, ax=axs_element, fraction=0.05)
         return img
 
     def plot_segm_img(self, img_data, fig, axs, row, col, title=None, cmap=None, auto_cbar=False, overlay=False, 
                       segm_cbar=True, slice_idx=None, alpha=None):
-
+        '''
+        Args:
+            img_data: loaded 2d or 3d nifti file
+            auto_cbar: automatically generate cbar if true
+            segm_cbar: show custom cbar with integer tickmarks based on the segments. if auto_cbar is True, this will not apply
+            overlay: if true, default cmap and alpha values will be generated (if not provided) to create a segment mask 
+        Returns: Plotted image
+        '''
+        
         if overlay is True and cmap is None:
             cmap = "gnuplot"
-            if alpha is None:
-                alpha = np.where(img_data == 0, 0, 0.4)
+        if overlay is True and alpha is None:
+            alpha = np.where(img_data == 0, 0, 0.4)
         
         if overlay is False and cmap is None:
             cmap = "turbo"
@@ -64,22 +72,25 @@ class MRIPlotter:
         label_cmap = plt.get_cmap(cmap, len(label_names))
 
         # re-use plot_struct_img but using cbar=false as default so we can manually build the discrete color bar
-        img = self.plot_struct_img(img_data, fig, axs, row, col, title, cmap=label_cmap, auto_cbar=(segm_cbar==False and auto_cbar), 
+        img = self.plot_struct_img(img_data, fig, axs, row, col, title=title,
+                                   cmap=label_cmap, auto_cbar=(segm_cbar==False and auto_cbar), 
                                    alpha=alpha, slice_idx=slice_idx)
 
+        axs_element = axs
+        if len(fig.axes) > 1:
+            axs_element = axs[col] if len(axs.shape) == 1 else axs[row, col]
+        
         # build the color bar
         if (auto_cbar == False and segm_cbar == True):  
             # note: axs can be 1d or 2d. if axs 2d and no. of rows =1, axs shape becomes (col,). 
             # if there are more than 1 rows, axs shape becmes (row, col)
-            axs_element = axs
-            if len(fig.axes) > 1:
-                axs_element = axs[col] if len(axs.shape) == 1 else axs[row, col]
-        
+            
             cbar = fig.colorbar(img, ax=axs_element, fraction=0.05)
             ticks = np.linspace(np.min(label_names), np.max(label_names)-1, len(label_names))
             cbar.set_ticks(ticks + 0.5)
             cbar.set_ticklabels(label_names)
-    
+            
+
     def get_largest_tumor_slice_idx(self, img_data):
         nonzero_counts = np.sum(np.count_nonzero(img_data, axis=0), axis=0 )
         slice_idx = np.argmax(nonzero_counts)
