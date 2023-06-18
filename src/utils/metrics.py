0e223146a6321cfa776cc6105feb2b36940ab7f3
Keith Pulmano <kp52@rice.edu>
2023-06-17 22:23:33 -0500

    enabled switching bet google storage and one drive

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -5,52 +5,59 @@ from scipy.ndimage import distance_transform_edt, binary_erosion, generate_binar
 from scipy.ndimage import _ni_support
 
 # region Commonly used metrics
-def calc_binary_metrics(pred_img, true_img, segment):
+def calc_binary_metrics(segm_pred, segm_true, segm_id):
     '''
     precision: tp / tp + fp => positive predicted value
     recall: tp / tp + fn => ratio of true positives to total positives in the data
     specificity = tn / (tn + fp) => ratio of true nagatives to total negatives in the data
     '''
     
-    true_img_bin = _convert_to_binary(true_img, segment)
-    pred_img_bin = _convert_to_binary(pred_img, segment)
+    mask_true = _get_binary_mask(segm_true, segm_id)
+    mask_pred = _get_binary_mask(segm_pred, segm_id)
 
-    recall = mdp.recall(pred_img_bin, true_img_bin)
-    specificity = mdp.specificity(pred_img_bin, true_img_bin)
+    recall = mdp.recall(mask_pred, mask_true)
+    specificity = mdp.specificity(mask_pred, mask_true)
     metrics_dict = {
-        "true_positive": np.count_nonzero(pred_img_bin & true_img_bin),
-        "true_negative": np.count_nonzero(~pred_img_bin & ~true_img_bin),
-        "false_positive": np.count_nonzero(pred_img_bin & ~true_img_bin),
-        "false_negative": np.count_nonzero(~pred_img_bin & true_img_bin),
-        "precision": mdp.precision(pred_img_bin, true_img_bin),
+        "true_positive": np.count_nonzero(mask_pred & mask_true),
+        "true_negative": np.count_nonzero(~mask_pred & ~mask_true),
+        "false_positive": np.count_nonzero(mask_pred & ~mask_true),
+        "false_negative": np.count_nonzero(~mask_pred & mask_true),
+        "precision": mdp.precision(mask_pred, mask_true),
         "recall": recall,
         "specificity": specificity,
         "sensitivity": recall,
         "true_positve_rate": recall,
-        "true_negative_rate": mdp.true_negative_rate(pred_img_bin, true_img_bin)
+        "true_negative_rate": mdp.true_negative_rate(mask_pred, mask_true)
     } 
     return metrics_dict
 
-def calc_dice_score(pred_img, true_img, segment):
-    # metric in medical image segmentation to evaluate similarity or overlap between segmented images
+def calc_dice_score(segm_pred, segm_true, segm_id):
+    # metric in medical image segm_idation to evaluate similarity or overlap between segm_ided images
     # convert images to binary
-    true_img_bin = _convert_to_binary(true_img, segment)
-    pred_img_bin = _convert_to_binary(pred_img, segment)
+    mask_true = _get_binary_mask(segm_true, segm_id)
+    mask_pred = _get_binary_mask(segm_pred, segm_id)
 
     # perform element wise multiplication to get intersection
-    intersection = np.sum(pred_img_bin * true_img_bin)
-    dice_score = (2.0 * intersection) / (np.sum(true_img_bin) + np.sum(pred_img_bin))
+    intersection = np.sum(mask_pred * mask_true)
+    dice_score = (2.0 * intersection) / (np.sum(mask_true) + np.sum(mask_pred))
     return dice_score
+  
+def calc_miou(mask_pred, mask_true):
+    num_i = len( np.where(mask_pred * mask_true != 0)[0] )
+    num_u = len( np.where(mask_pred + mask_true != 0)[0] )
+    miou = num_i / num_u * 100
+    return miou, num_u
+  
 # endregion
 
 # region Boundary-based metrics
-def calc_hausdorff_95(pred_img, true_img, segment):
-    return _exec_hausdorff_95(pred_img, true_img, segment, plot=False)
+def calc_hausdorff_95(segm_pred, segm_true, segm_id):
+    return _exec_hausdorff_95(segm_pred, segm_true, segm_id, plot=False)
 
-def plot_hausdorff_95(pred_img, true_img, segment):
-    return _exec_hausdorff_95(pred_img, true_img, segment, plot=True)
+def plot_hausdorff_95(segm_pred, segm_true, segm_id):
+    return _exec_hausdorff_95(segm_pred, segm_true, segm_id, plot=True)
 
-def _calc_surface_distances(pred_img_bin, true_img_bin, voxelspacing=None, connectivity=1):
+def _calc_surface_distances(mask_pred, mask_true, voxelspacing=None, connectivity=1):
   """
   The distances between the surface voxel of binary objects in result and their
   nearest partner surface voxel of a binary object in reference.
@@ -60,36 +67,36 @@ def _calc_surface_distances(pred_img_bin, true_img_bin, voxelspacing=None, conne
   if voxelspacing is not None:
       # ensures that the sequence has the same length as the number of dimensions in the result array
       # not needed for now
-      voxelspacing = _ni_support._normalize_sequence(voxelspacing, pred_img_bin.ndim)
+      voxelspacing = _ni_support._normalize_sequence(voxelspacing, mask_pred.ndim)
       voxelspacing = np.asarray(voxelspacing, dtype=np.float64)
       if not voxelspacing.flags.contiguous:
           voxelspacing = voxelspacing.copy()
           
   # this will create 3x3 binary array with False values at the center
   # and its surrounding structures. diagonals are not considered if connectivity = 1
-  footprint = generate_binary_structure(pred_img_bin.ndim, connectivity)
+  footprint = generate_binary_structure(mask_pred.ndim, connectivity)
 
   # extract only 1-pixel border line of objects
   # applies binary erosion to the image
-  pred_img_border = pred_img_bin ^ binary_erosion(pred_img_bin, structure=footprint, iterations=1)
-  true_img_border = true_img_bin ^ binary_erosion(true_img_bin, structure=footprint, iterations=1)
+  segm_pred_border = mask_pred ^ binary_erosion(mask_pred, structure=footprint, iterations=1)
+  segm_true_border = mask_true ^ binary_erosion(mask_true, structure=footprint, iterations=1)
 
   # calculate distances from each voxel to the closest border 
-  dist = distance_transform_edt(~true_img_border, sampling=voxelspacing)
-  surface_dist = np.where(pred_img_border, dist, None)
+  dist = distance_transform_edt(~segm_true_border, sampling=voxelspacing)
+  surface_dist = np.where(segm_pred_border, dist, None)
   return surface_dist
 
-def _exec_hausdorff_95(pred_img, true_img, segment, plot=False):
+def _exec_hausdorff_95(segm_pred, segm_true, segm_id, plot=False):
   # convert image to binary
-  true_img_bin = _convert_to_binary(true_img, segment)
-  pred_img_bin = _convert_to_binary(pred_img, segment)
+  mask_true = _get_binary_mask(segm_true, segm_id)
+  mask_pred = _get_binary_mask(segm_pred, segm_id)
 
-  if np.sum(true_img_bin) + np.sum(pred_img_bin) == 0:
-    raise Exception("Images do not have common segments")
+  if np.sum(mask_true) + np.sum(mask_pred) == 0:
+    raise Exception("Images do not have common segment ids")
 
-  connectivity = pred_img_bin.ndim * 2
-  surface_dist_pred = _calc_surface_distances(pred_img_bin, true_img_bin, connectivity=connectivity)
-  surface_dist_true = _calc_surface_distances(true_img_bin, pred_img_bin, connectivity=connectivity)
+  connectivity = mask_pred.ndim * 2
+  surface_dist_pred = _calc_surface_distances(mask_pred, mask_true, connectivity=connectivity)
+  surface_dist_true = _calc_surface_distances(mask_true, mask_pred, connectivity=connectivity)
   surface_dist_pred_no_null = surface_dist_pred[surface_dist_pred != None]
   surface_dist_true_no_null = surface_dist_true[surface_dist_true != None]
   hd95_val = np.percentile(np.hstack((surface_dist_pred_no_null, surface_dist_true_no_null)), 95)
@@ -102,7 +109,6 @@ def _exec_hausdorff_95(pred_img, true_img, segment, plot=False):
 # endregion
 
 # region Helper Functions
-def _convert_to_binary(img, segment):
-    return img == segment    
-
+def _get_binary_mask(img, segm_id):
+    return img == segm_id
 # endregion
