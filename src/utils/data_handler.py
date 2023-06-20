6f0842b81e8e12ee8950eab4c13a92fae20a5769
Keith Pulmano <kp52@rice.edu>
2023-06-20 01:44:11 -0500

    reusable function for loading image and integration to maskformer

---
+++ src/utils/data_handler.py
@@ -0,0 +1,405 @@
+import os
+import tempfile
+import time
+from enum import Enum
+from io import BytesIO
+from typing import Union
+import shutil
+
+import nibabel as nib
+import numpy as np
+
+import importlib
+import utils.google_storage as gs
+importlib.reload(gs)
+
+from utils.google_storage import GStorageClient
+from onedrivedownloader import download
+    
+# All constants from source directories
+# Define One Drive urls and file names. files are similar in google storage but unzipped
+IMAGES_STRUCTURAL_URL = "https://rice-my.sharepoint.com/:u:/g/personal/hl9_rice_edu/ER8oOEAm1ANGlK4sodUPdX0B6_7IxmbRoneyo-RXI2HYOg"
+IMAGES_STRUCTURAL_FNAME = "images_structural"
+
+IMAGES_ANNOT_URL = "https://rice-my.sharepoint.com/:u:/g/personal/hl9_rice_edu/EbmrLEe1ZgpNkaujtMtlDIEB9rQ0Zj82dOWIttA8sD5lSg"
+IMAGES_ANNOT_FNAME = "images_segm" 
+
+IMAGES_ANNOT_REDUCED_URL = "https://rice-my.sharepoint.com/:u:/g/personal/hl9_rice_edu/EfqrvokObOJEhovsqLl_sscBgplo836OUdN5kJzAYqPZyg"
+IMAGES_ANNOT_REDUCED_FNAME = "images_annot_reduced"
+
+IMAGES_AUTO_SEGM_URL = "https://rice-my.sharepoint.com/:u:/g/personal/hl9_rice_edu/EToY-Cli4vxMqYwHx_NZ4JsBi1Lo8tOskj9zb4_AZmDfcg"
+IMAGES_AUTO_SEGM_FNAME = "automated_segm"
+
+IMAGES_AUTO_SEGM_REDUCED_URL = "https://rice-my.sharepoint.com/:u:/g/personal/hl9_rice_edu/EXwqKvC8QpBBjFQUXzKR1-IBtJeP1hwXUQAoJOneJx4-Hw"
+IMAGES_AUTO_SEGM_REDUCED_FNAME = "automated_segm_reduced"
+
+IMAGES_TRAIN_2D_URL = "https://rice-my.sharepoint.com/:u:/g/personal/hl9_rice_edu/ETxkBw8cJS9InmWhUUFx0l0BnAES2uSbCaLn0jrMi-HK3Q"
+IMAGES_TRAIN_2D_FNAME = "train_2d"
+ 
+IMAGES_VAL_2D_URL   = "https://rice-my.sharepoint.com/:u:/g/personal/hl9_rice_edu/EeLd0vd4IuxBn5hPlzO8gz0BT2kDX9Uo9AkNqxR_sjKbMg"
+IMAGES_VAL_2D_FNAME = "val_2d" 
+
+IMAGES_TRAIN_2D_CROSS_URL = "https://rice-my.sharepoint.com/:u:/g/personal/hl9_rice_edu/EWnXLCwsONBNpzziTciJXt4B68qkcVfZTfSXRcS9gxWWGQ?e=r5xDU4"
+IMAGES_TRAIN_2D_CROSS_FNAME = "train_2d_cross"
+ 
+IMAGES_VAL_2D_CROSS_URL   = "https://rice-my.sharepoint.com/:u:/g/personal/hl9_rice_edu/EdKtKyMfy2lJvdGGSSz23yEB34_C4MiEm2BmRE9q451Lgg?e=9dK2NO"
+IMAGES_VAL_2D_CROSS_FNAME = "val_2d_cross" 
+
+# All constants for destination directories
+# this will be either in google storage or runtime depending on flag
+DATA_DIR = "content/data"
+
+# this will be either in google storage or local: src/training
+TRAIN_DIR = "training"
+
+SEGMENTS = {
+    0: "ELSE",
+    1: "NCR",
+    2: "ED",
+    4: "ET"
+}
+
+class StructuralScan(str, Enum):
+    T1 = "T1"
+    T2 = "T2"
+    T1GD = "T1GD"
+    FLAIR = "FLAIR"
+
+class MriType(Enum):
+    STRUCT_SCAN = 1
+    AUTO_SEGMENTED = 2
+    ANNOTATED = 3
+    STRUCT_SCAN_REDUCED = 4
+    AUTO_SEGMENTED_REDUCED = 5
+    ANNOTATED_REDUCED = 6
+    TRAIN_2D = 7
+    VAL_2D = 8
+    TRAIN_2D_CROSS = 9
+    VAL_2D_CROSS = 10
+    
+class DataHandler:        
+    def __init__(self, enable_gstorage=True):
+        """Initialize MriImage with a parameter.
+        Args:
+            enable_gstorage (int): use runtime storage if set to false
+        """
+        
+        self.google_client = GStorageClient()
+        self.enable_gstorage = enable_gstorage
+        self.train_dir = TRAIN_DIR
+        
+        if self.enable_gstorage:
+            # google storage needs to see the path as a directory explicity
+            self.data_dir = DATA_DIR + "/"
+        else:
+            # runtime drive needs absolute path, hence the prefix
+            self.data_dir = "/" + DATA_DIR
+            os.makedirs(self.data_dir, exist_ok=True)
+            
+            # manually create training directory. training dir will be within the repository
+            os.makedirs(self.train_dir, exist_ok=True)
+    
+    def save_from_source_path(self, file_name, source_path, train_dir_prefix = None, data_dir_prefix = None, upload_to_cloud=False):
+        destination_path = self._get_upload_dir(file_name, train_dir_prefix, data_dir_prefix)
+        
+        if self.enable_gstorage:
+            self.google_client.save_from_source_path(source_path, destination_path)
+            os.remove(source_path)
+        else:
+            if upload_to_cloud:
+                self.google_client.save_from_source_path(source_path, destination_path)
+
+            # move local temp file to correct directory
+            shutil.move(source_path, destination_path)
+            
+        
+    def save_from_text(self, file_name, data, train_dir_prefix = None, data_dir_prefix = None):
+        destination_path = self._get_upload_dir(file_name, train_dir_prefix, data_dir_prefix)
+        
+        if self.enable_gstorage:
+            self.google_client.save_from_text(destination_path, data)
+        else:
+            with open(destination_path, 'w') as file:
+                file.write(data)
+    
+    def load_text(self, file_name: str, dir_prefix: str = "", absolute_dir: str = None):
+        # load from training folder by default
+        source_path = os.path.join(self.train_dir, dir_prefix, file_name)
+        
+        # allow absolute directory if needed
+        if absolute_dir is not None:
+            source_path = os.path.join(absolute_dir, file_name)
+        
+        if self.enable_gstorage:    
+            # create temp file path with same file format as file being downloaded
+            destination_path = self.create_temp_file(source_path)
+            
+            # download file and save to temp file path created
+            self.google_client.download_blob_as_file(source_path, destination_path)
+            source_path = destination_path
+    
+        # read the temp file if from gstorage
+        # if runtime, open the file directly
+        lines = []
+        with open(source_path, 'r') as file:
+            for line in file:
+                lines.append(line.strip())
+        return lines
+
+    def load_mri(self, subj_id: str, mri_type: MriType, file_no: int = None, struct_scan: Union[StructuralScan, None] = None, 
+                 img_dir: str = None, return_nifti: bool = False, dtype: str = None):
+        """
+        Read MRI data from the specified subject file.
+
+        Args:
+            subj_file (str): The path or filename of the MRI subject file (eg. UPENN-GBM-00312_1)
+            mri_type (Mri_Type): The type of MRI data being read (e.g. annotated, autosegmented, reduced). Strictly use ENUM class for valid types
+            struct_scan (Struct_Scan): The type of structural scan to load (e.g T1, T2, T1GD, FLAIR). Strictly use ENUM class for valid structural scans
+            return_nifti (bool, optional): Flag indicating whether raw nifit. Defaults to False.
+            dtype (str, optional): The data type of the MRI data e.g. uint16, uint8. If none, default is float.
+
+        Returns:
+            The MRI data in the specified format.
+        """
+         
+        # initialize data
+        destination_path = None
+        nifti = None
+        
+        # construct the full file path using a helper function
+        mri_file_path = self._get_full_path(subj_id=subj_id, mri_type=mri_type, struct_scan=struct_scan, file_no=file_no)
+       
+        # overwrite constructed file if img_dir is supplied
+        if img_dir is not None:
+            mri_file_path = img_dir
+             
+      
+        if self.enable_gstorage:    
+            # google storage way
+            # create temp file path with same file format as file being downloaded
+            destination_path = self.create_temp_file(mri_file_path)
+            
+            self.google_client.download_blob_as_file(mri_file_path, destination_path)
+            nifti = nib.load(destination_path)
+
+        else:
+            # one drive way
+            self.download_from_onedrive(mri_type=mri_type)
+            nifti = nib.load(mri_file_path)
+
+      
+        # return uint if file is segmentation not specified
+        if struct_scan is None and dtype is None:
+            dtype="uint"
+
+        # get array data
+        data = None
+        if dtype is not None:
+            data = (nifti.get_fdata()).astype(dtype)
+        else:
+            data = nifti.get_fdata()
+        
+        # delete temporary file
+        if self.enable_gstorage:
+            os.remove(destination_path)
+        
+        if return_nifti:
+            return data, nifti
+        else:
+            return data
+    
+    def list_dir(self, sort: bool=True, dir_prefix: str = "", absolute_dir: str = None):
+        # by default, save inside training folder
+        dir = os.path.join(self.train_dir, dir_prefix)
+        if absolute_dir is not None:
+            dir = absolute_dir
+        
+        all_files = []
+        if self.enable_gstorage:
+            all_files = self.google_client.list_blob_in_dir(dir)
+        else:
+            all_files = os.listdir(dir)
+        
+        if sort:
+            all_files.sort()
+        return all_files
+        
+    def list_mri_in_dir(self, mri_type: MriType, sort: bool=True):
+        
+        dir = self._get_directory(mri_type=mri_type)
+        if self.enable_gstorage == False:
+            # attempt to download files in runtime first
+            self.download_from_onedrive(mri_type=mri_type)
+            
+        return self.list_dir(absolute_dir=dir, sort=sort)
+
+    def get_mri_subj_id(self, file_name):
+        # file_name: UPENN-GBM-00006_11_FLAIR_1.nii.gz
+        # subj_id: 00006
+        if file_name.strip().split('.')[-1] == 'gz':
+            return file_name.strip().split('_')[0].split('-')[-1]
+        return None
+    
+    def get_mri_subj_file(self, file_name):
+        # file_name: UPENN-GBM-00006_11_FLAIR_1.nii.gz
+        # result: UPENN-GBM-00006_1.nii.gz
+        if file_name.strip().split('.')[-1] == 'gz':
+            return file_name.strip().split('_')[0] + \
+                        "_" + file_name.strip().split('_')[3]
+        return None
+    
+    
+    def get_mri_file_no(self, file_name):
+        # file_name: UPENN-GBM-00006_11_FLAIR_1.nii.gz
+        # file_no: 11
+        if file_name.strip().split('.')[-1] == 'gz':
+            return int(file_name.strip().split('.')[0].split('_')[-1])
+        return None
+    
+    def get_largest_tumor_slice_idx(self, img_data, sum=False):
+        non_zero_x = np.count_nonzero(img_data, axis=0)
+        if sum is True:
+            non_zero_x = np.sum(img_data, axis=0)
+        total_y = np.sum(non_zero_x, axis=0 )
+        slice_idx = np.argmax(total_y)
+        return slice_idx, total_y[slice_idx]
+    
+    def download_from_onedrive(self, mri_type: MriType):
+        
+        directory = self._get_directory(mri_type=mri_type)
+        filename = directory + ".zip"
+        
+        if not os.path.exists(filename):
+            # Note: For some MRI folder structures, unzipping results to another extra subdirectory called /data
+            # specify unzip path parameter to put the files in the same level as the others
+            # eg: data/images_annot_reduced instead of data/data/images_annot_reduced
+            
+            # TODO: force unizp path for now to match google storage: /content/data/2D_slices_reduced_norm/data/
+            dir_2d_slices = os.path.join(self.data_dir, "2D_slices_reduced_norm")
+            
+            if mri_type == MriType.STRUCT_SCAN:
+                download(IMAGES_STRUCTURAL_URL, filename=filename)
+            
+            elif mri_type == MriType.AUTO_SEGMENTED: 
+                download(IMAGES_AUTO_SEGM_URL, filename=filename)
+                
+            elif mri_type == MriType.ANNOTATED:
+                download(IMAGES_ANNOT_URL, filename=filename)
+                
+            elif mri_type == MriType.STRUCT_SCAN_REDUCED:
+                download(IMAGES_ANNOT_REDUCED_URL, filename=filename, unzip_path=self.data_dir)
+                
+            elif mri_type == MriType.AUTO_SEGMENTED_REDUCED:
+                download(IMAGES_AUTO_SEGM_REDUCED_URL, filename=filename)
+                
+            elif mri_type == MriType.ANNOTATED_REDUCED:
+                download(IMAGES_ANNOT_REDUCED_URL, filename=filename, unzip_path=self.data_dir)
+                
+            elif mri_type == MriType.TRAIN_2D:
+                download(IMAGES_TRAIN_2D_URL, filename=filename, unzip_path=dir_2d_slices)
+
+            elif mri_type == MriType.VAL_2D:
+                download(IMAGES_VAL_2D_URL, filename=filename, unzip_path=dir_2d_slices)
+
+            elif mri_type == MriType.TRAIN_2D_CROSS:
+                download(IMAGES_TRAIN_2D_CROSS_URL, filename=filename, unzip_path=dir_2d_slices)
+            
+            elif mri_type == MriType.VAL_2D_CROSS: 
+                download(IMAGES_VAL_2D_CROSS_URL, filename=filename, unzip_path=dir_2d_slices)
+            
+    def _get_full_path(self, subj_id: str, mri_type: MriType, file_no: int = None, struct_scan: Union[StructuralScan, None] = None):
+        file_name = f"{subj_id}"
+        file_dir = self._get_directory(mri_type=mri_type)
+        
+        # build file name and supply the file path based on current onedrive folder structure
+        # the case of structural images is different since each subject has its own folder
+        
+        if mri_type == MriType.STRUCT_SCAN:
+            file_name = f"{file_name}_{struct_scan}"
+            file_dir = os.path.join(file_dir, subj_id)
+        
+        elif mri_type == MriType.AUTO_SEGMENTED: 
+            file_name = f"{file_name}_automated_approx_segm"
+            
+        elif mri_type == MriType.ANNOTATED:
+            file_name = f"{file_name}_segm"
+        
+        elif mri_type == MriType.STRUCT_SCAN_REDUCED:
+            file_name = f"{file_name}_{struct_scan}_cut"
+            
+        elif mri_type == MriType.AUTO_SEGMENTED_REDUCED:
+            file_name = f"{file_name}_automated_approx_segm_cut"
+            
+        elif mri_type == MriType.ANNOTATED_REDUCED:
+            file_name = f"{file_name}_segm_cut"
+            
+        elif mri_type == MriType.TRAIN_2D or mri_type == MriType.VAL_2D or MriType.TRAIN_2D_CROSS or MriType.VAL_2D_CROSS:
+            if struct_scan is not None:
+                file_name = f"{subj_id}_11_{struct_scan}_{file_no}"
+            else:
+                file_name = f"{subj_id}_11_segm_{file_no}"
+            
+        file_name = f"{file_name}.nii.gz"
+        f_path = os.path.join(file_dir, file_name)
+       
+        return f_path
+
+    def _get_directory(self, mri_type: MriType):
+        dir_2d_slices = os.path.join(self.data_dir, "2D_slices_reduced_norm", "data")
+        
+        if mri_type == MriType.STRUCT_SCAN:
+            return os.path.join(self.data_dir, IMAGES_STRUCTURAL_FNAME)
+        
+        elif mri_type == MriType.AUTO_SEGMENTED:
+            return os.path.join(self.data_dir, IMAGES_AUTO_SEGM_FNAME)
+
+        elif mri_type == MriType.ANNOTATED:
+            return os.path.join(self.data_dir, IMAGES_ANNOT_FNAME)
+            
+        elif mri_type == MriType.STRUCT_SCAN_REDUCED:
+            # same as IMAGES_ANNOT_REDUCED_FNAME
+            return os.path.join(self.data_dir, IMAGES_ANNOT_REDUCED_FNAME)
+
+        elif mri_type == MriType.AUTO_SEGMENTED_REDUCED:
+            return os.path.join(self.data_dir, IMAGES_AUTO_SEGM_REDUCED_FNAME)
+
+        elif mri_type == MriType.ANNOTATED_REDUCED:
+            return os.path.join(self.data_dir, IMAGES_ANNOT_REDUCED_FNAME)
+
+        elif mri_type == MriType.TRAIN_2D:
+            return os.path.join(dir_2d_slices, IMAGES_TRAIN_2D_FNAME)
+
+        elif mri_type == MriType.VAL_2D:
+            return os.path.join(dir_2d_slices, IMAGES_VAL_2D_FNAME)
+
+        elif mri_type == MriType.TRAIN_2D_CROSS:
+            return os.path.join(dir_2d_slices, IMAGES_TRAIN_2D_CROSS_FNAME)
+        
+        elif mri_type == MriType.VAL_2D_CROSS:
+            return os.path.join(dir_2d_slices, IMAGES_VAL_2D_CROSS_FNAME)
+    
+    def create_temp_file(self, file_path):
+        suffix = self._get_blob_extension(file_path)
+        with tempfile.NamedTemporaryFile(suffix=suffix, delete=False) as temp_file:
+            return temp_file.name
+        
+    def _get_blob_extension(self, file_path):
+        suffix = os.path.splitext(file_path)
+        if file_path.endswith(".nii.gz"):
+            return ".nii.gz"
+        else: 
+            return suffix[1]
+        
+    def _get_upload_dir(self, file_name, train_dir_prefix = None, data_dir_prefix = None):
+        file_path = None
+    
+        if data_dir_prefix is not None:
+            # allow data directory prefix eg. /content/data/myprefix
+            file_path = os.path.join(self.data_dir, data_dir_prefix, file_name)
+        else:
+            # allow train directory prefix  eg. training/myprefix
+            if train_dir_prefix is None:
+                train_dir_prefix = ""
+            file_path = os.path.join(self.train_dir, train_dir_prefix, file_name)
+        
+        return file_path
--- src/utils/google_storage.py
+++ src/utils/google_storage.py
@@ -1,5 +1,4 @@
 import os
-import tempfile
 
 from google.cloud import storage as gcs
 
@@ -12,35 +11,36 @@ class GStorageClient:
         # authenticate storage client if specified. if not, runtime will automatically be used
         self.storage_client = gcs.Client.from_service_account_json(STORAGE_AUTH_FILE)
         
-        
-    def download_blob_as_file(self, blob_path):
-        # Create a temporary file
-        with tempfile.NamedTemporaryFile(suffix=".nii.gz", delete=False) as temp_file:
-            temp_file_path = temp_file.name
-
-        # get bucket instance
-        bucket = self.storage_client.get_bucket(STORAGE_BUCKET_NAME)
-        blob = bucket.blob(blob_path)
-        blob.download_to_filename(temp_file_path)
-        return temp_file_path
+    def download_blob_as_file(self, source_path, destination_path):
+        # save the blob into the temp file path
+        blob = self._get_blob_client(source_path)
+        blob.download_to_filename(destination_path)
     
-    def save_to_blob(self, blob_path, temp_file_path):
-        bucket = self.storage_client.get_bucket(STORAGE_BUCKET_NAME)
-
-        # Create a Blob object in the bucket
-        blob = bucket.blob(blob_path)
-        # Path of the local file
-        blob.upload_from_filename(temp_file_path)
+    def save_from_source_path(self, source_path, destination_path):
+        # Upload temp_file_path to blob path
+        blob = self._get_blob_client(destination_path)   
+        blob.upload_from_filename(source_path)
         
+    def save_from_text(self, destination_path, text):
+        blob = self._get_blob_client(destination_path)   
+        blob.upload_from_string(text)
+            
     def list_blob_in_dir(self, prefix):
         bucket = gcs.Bucket(self.storage_client, STORAGE_BUCKET_NAME)
+        prefix = prefix + "/"
         
         # list all files in the directory
         blob_paths = self.storage_client.list_blobs(bucket, prefix=prefix)
         blobs = [os.path.basename(blob.name) for blob in blob_paths]
         
-        # probably a mac issue when moving files to google cloud. just delete it from the list of files
+        # probably a mac issue. file is created when moving files to google cloud. 
+        # just delete it from the list of files
         if ".DS_Store" in blobs:
             blobs.remove(".DS_Store")
                 
         return blobs
+    
+    def _get_blob_client(self, blob_path):
+        bucket = self.storage_client.get_bucket(STORAGE_BUCKET_NAME)
+        blob = bucket.blob(blob_path)
+        return blob
