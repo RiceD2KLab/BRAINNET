fix hd95. use package calculation instead

src/utils/metrics.py
@@ -1,6 +1,7 @@
 import medpy.metric.binary as mdp
-import numpy as np
 
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy.ndimage import distance_transform_edt, binary_erosion, generate_binary_structure
 from scipy.ndimage import _ni_support
 
@@ -47,7 +48,14 @@ def calc_dice_score(segm_pred, segm_true, segm_id=None):
     dice_score = (2.0 * intersection) / (np.sum(mask_true) + np.sum(mask_pred))
     return dice_score
   
-def calc_miou(mask_pred, mask_true):
+def calc_miou(segm_pred, segm_true, segm_id=None):
+    mask_true = segm_true
+    mask_pred = segm_pred
+    
+    if segm_id is not None:
+        mask_true = _get_binary_mask(segm_true, segm_id)
+        mask_pred = _get_binary_mask(segm_pred, segm_id)
+        
     num_i = len( np.where(mask_pred * mask_true != 0)[0] )
     num_u = len( np.where(mask_pred + mask_true != 0)[0] )
     miou = num_i / num_u * 100
@@ -57,49 +65,56 @@ def calc_miou(mask_pred, mask_true):
 
 # region Boundary-based metrics
 def calc_hausdorff_95(segm_pred, segm_true, segm_id=None):
-    return mdp.hd95(segm_pred, segm_true)
-    # return _exec_hausdorff_95(segm_pred, segm_true, segm_id=segm_id, plot=False)
+    return _exec_hausdorff_95(segm_pred, segm_true, segm_id=segm_id, plot=False)
 
 def plot_hausdorff_95(segm_pred, segm_true, segm_id=None):
     return _exec_hausdorff_95(segm_pred, segm_true, segm_id=segm_id, plot=True)
 
-def _calc_surface_distances(mask_pred, mask_true, voxelspacing=None, connectivity=1):
+def _calc_surface_distances(mask_pred, true_pred, voxelspacing=None, connectivity=1):
     """
     The distances between the surface voxel of binary objects in result and their
     nearest partner surface voxel of a binary object in reference.
 
     Source Code: https://github.com/loli/medpy/blob/master/medpy/metric/binary.py#L357
     """
+    mask_pred = np.atleast_1d(mask_pred.astype(np.bool))
+    true_pred = np.atleast_1d(true_pred.astype(np.bool))
     if voxelspacing is not None:
-        # ensures that the sequence has the same length as the number of dimensions in the result array
-        # not needed for now
         voxelspacing = _ni_support._normalize_sequence(voxelspacing, mask_pred.ndim)
         voxelspacing = np.asarray(voxelspacing, dtype=np.float64)
         if not voxelspacing.flags.contiguous:
             voxelspacing = voxelspacing.copy()
-            
+
     # this will create 3x3 binary array with False values at the center
     # and its surrounding structures. diagonals are not considered if connectivity = 1
+    # binary structure
     footprint = generate_binary_structure(mask_pred.ndim, connectivity)
-
+    
+    # test for emptiness
+    if 0 == np.count_nonzero(mask_pred): 
+        raise RuntimeError('The first supplied array does not contain any binary object.')
+    if 0 == np.count_nonzero(true_pred): 
+        raise RuntimeError('The second supplied array does not contain any binary object.')    
+    
     # extract only 1-pixel border line of objects
     # applies binary erosion to the image
-    segm_pred_border = mask_pred ^ binary_erosion(mask_pred, structure=footprint, iterations=1)
-    segm_true_border = mask_true ^ binary_erosion(mask_true, structure=footprint, iterations=1)
+    pred_border = mask_pred ^ binary_erosion(mask_pred, structure=footprint, iterations=1)
+    true_border = true_pred ^ binary_erosion(true_pred, structure=footprint, iterations=1)
+    
+    # calculate surface distance of each pixel 
+    dist = distance_transform_edt(~true_border, sampling=voxelspacing)
 
-    # calculate distances from each voxel to the closest border 
-    dist = distance_transform_edt(~segm_true_border, sampling=voxelspacing)
-    sds = dist[segm_pred_border]
-        
-    return sds
-    #   surface_dist = np.where(segm_pred_border, dist, None)
-    #   return surface_dist
+    # compute average surface distance        
+    # Note: scipys distance transform is calculated only inside the borders of the
+    #       foreground objects, therefore the input has to be reversed
+    surface_dist = np.where(pred_border, dist, -1)
+    return surface_dist
 
 
 def _exec_hausdorff_95(segm_pred, segm_true, segm_id=None, plot=False):
     mask_true = segm_true
     mask_pred = segm_pred
-
+    
     if segm_id is not None:
         mask_true = _get_binary_mask(segm_true, segm_id)
         mask_pred = _get_binary_mask(segm_pred, segm_id)
@@ -107,18 +122,23 @@ def _exec_hausdorff_95(segm_pred, segm_true, segm_id=None, plot=False):
     if np.sum(mask_true) + np.sum(mask_pred) == 0:
         print("Both images do not have the segment ids")
         return None
-
-    connectivity = mask_pred.ndim * 2
-    surface_dist_pred = _calc_surface_distances(mask_pred, mask_true, connectivity=connectivity)
-    surface_dist_true = _calc_surface_distances(mask_true, mask_pred, connectivity=connectivity)
-    # surface_dist_pred_no_null = surface_dist_pred[surface_dist_pred != None]
-    # surface_dist_true_no_null = surface_dist_true[surface_dist_true != None]
-    hd95_val = np.percentile(np.hstack((surface_dist_true, surface_dist_true)), 95)
-
+    
+    connectivity=1
+    if mask_true.ndim == 3:
+        connectivity = 26
+    elif mask_true.ndim == 2:
+        connectivity = 8
+         
     if plot:
+        all_dist_pred = _calc_surface_distances(mask_pred, mask_true, connectivity=connectivity)
+        all_dist_true = _calc_surface_distances(mask_true, mask_pred, connectivity=connectivity)
+        surface_dist_pred = all_dist_pred[all_dist_pred >= 0]
+        surface_dist_true = all_dist_true[all_dist_true >= 0]
+        
+        hd95_val = np.percentile(np.hstack((surface_dist_true, surface_dist_pred)), 95)
         return hd95_val, surface_dist_pred, surface_dist_true
     else:
-        return hd95_val
+        return mdp.hd95(mask_pred, mask_true, connectivity=connectivity)
 
 # endregion
 