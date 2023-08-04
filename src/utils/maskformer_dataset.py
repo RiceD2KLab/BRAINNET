1a9796e38120c889cca28bde10827115d9dfc268
Ben Dowdell <ben.dowdell87@gmail.com>
2023-08-04 15:26:03 -0500

    implements minimal augmentations in MaskformerMRIDataset

--- src/utils/maskformer_dataset.py
+++ src/utils/maskformer_dataset.py
@@ -4,7 +4,7 @@ import torch
 import torchvision
 import torchvision.transforms.functional as TF
 
-from PIL import Image
+from PIL import Image, ImageEnhance
 from torch import from_numpy
 from torch.utils.data import Dataset
 from typing import List, Literal
@@ -164,12 +164,18 @@ class MaskformerMRIDataset(Dataset):
         if self.augment is True:
 
             # # Image Color Jittering
-            # print("image mean, max=",image[:,:,0].mean(), image[:,:,0].max())
             pil_image = Image.fromarray(image.astype(np.uint8))
-            color_jitter = torchvision.transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1)
-            pil_image = color_jitter(pil_image)
+#            color_jitter = torchvision.transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1)
+#            pil_image = color_jitter(pil_image)
+
+            # apply contrast
+            if random.random() <= 0.5:
+                gamma_factor = 1.6
+                enhancer = ImageEnhance.Contrast(pil_image)
+                pil_image = enhancer.enhance(gamma_factor)
+
+            # convert back to numpy
             image = np.array(pil_image)
-            # print("image mean, max=",image[:,:,0].mean(), image[:,:,0].max())
 
             # convert to C, H, W (torchvision transforms assume this shape)
             image = image.transpose(2,0,1)
@@ -180,16 +186,15 @@ class MaskformerMRIDataset(Dataset):
             # convert to tensors
             image = torch.from_numpy(image.astype('float'))
             instance_seg = torch.from_numpy(instance_seg)
-            # print("image mean, max=",image[0,:,:].mean(), image[0,:,:].max())
 
             # Apply random horizontal flip to image and mask
-            if np.random.random() > 0.5:
+            if random.random() <= 0.3:
                 image = TF.hflip(image)
                 instance_seg = TF.hflip(instance_seg)
 
             # Apply random crop to both the image and mask (as tensors)
             factor1 = 0.8
-            if random.random() > 0.2:
+            if random.random() <= 0.2:
                 chance1 = random.choice([0,1,2,3,4])
                 dim1 = image.shape
                 # print('Original image dimension:',dim1,'choice:',chance1)
