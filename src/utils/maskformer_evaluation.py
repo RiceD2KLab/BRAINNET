789af7c1af5b64f9ee91dfbcbfe45a93ca01a3c6
Ben Dowdell <ben.dowdell87@gmail.com>
2023-08-08 16:26:32 -0500

    adds docstrings to methods with missing docstring

--- src/utils/maskformer_evaluation.py
+++ src/utils/maskformer_evaluation.py
@@ -19,45 +19,87 @@ from utils.maskformer_inference import MaskFormerInference
 class MaskFormerEvaluation():
     # utility functions
     def __init__(self, use_brats_region=False):
+        """
+        Defines evaluation metrics and visualizations for use with MaskFormer
+
+        Args:
+            use_brats_region (bool): flag indicating whether to use BRATS challenge
+            segmentation labels (TC, WT, ET), or native data segmentation labels
+            (NCR, ET, ED, ELSE)
+        """
         self.use_brats_region = use_brats_region
         self.all_label_colors = list(mri_common.SEGMENT_COLORS.values())
         self.mri_plt = MRIPlotter()
-        
+
         if self.use_brats_region:
             self.all_label_names = list(mri_common.BRATS_REGIONS.keys())
         else:
             self.all_label_names = list(mri_common.SEGMENTS.values())
 
     def calc_metrics(self, subj_id: str, mask_pred_binary, mask_true_binary):
-        
+        """
+        Calculates metrics for a given subject using the predicted mask and
+        ground truth mask.
+
+        Metrics are:
+        1) Dice Score
+        2) 95th percentile Hausdorff Distance
+        3) Common: Precision, Recall, Sensitivity, & Specificity
+
+        Args:
+            subj_id (str): The unique subject identifier (eg. UPENN-GBM-00312)
+            mask_pred_binary (numpy.ndarray): predicted binary mask
+            mask_true_binary (numpy.ndarray): ground truth binary mask
+
+        Returns:
+            tuple of float, float, and dictionary of "common" metrics
+        """
+
         print("Calculating metrics for ", subj_id)
         # dice coefficient
         # output array of dice score for all segments: [0.9, 0.8, 0.92, 0.3]
         dice_score = self._calc_metric_all_segments(self.all_label_names, metrics.calc_dice_score, mask_pred_binary, mask_true_binary)
-        
+
         # 95% hausdorff distance
         # output array of hd95 for all segments: [5.3, 2.8, 3.92, 1]
         hausdorff_val = self._calc_metric_all_segments_pool(self.all_label_names, metrics.calc_hausdorff_95, mask_pred_binary, mask_true_binary)
-        
+
         # common metrics
         # e.g. 'true_positive': [20927582, 148267, 687623, 226880]
         common_metrics = self._calc_metric_all_segments(self.all_label_names, metrics.calc_binary_metrics, mask_pred_binary, mask_true_binary)
         common_metrics_dict = {}
         for key in common_metrics[0]:
             common_metrics_dict[key] = [metric[key] for metric in common_metrics]
-        
+
         return dice_score, hausdorff_val, common_metrics_dict
-                
+
     def predict_and_eval(self, subj_names: List[str], mf_inference: MaskFormerInference, data_handler: DataHandler,
                         metrics_dir_prefix: str, metrics_file_name: str, recalculate: bool, batch_size=10):
-        
+        """
+        Passes input images through trained MaskFormer to predict segmentation masks and then
+        calculates metrics
+
+        Args:
+           subj_names (list): list of strings containing subject names
+           mf_inference (MaskFormerInference): instance of MaskFormerInference (train/val/test)
+           data_handler (DataHandler): instance of DataHandler (train/val/test)
+           metrics_dir_prefix (str): path to save metrics outputs
+           metrics_file_name (str): output file name for saving metrics
+           recalculate (bool): flag indicating whether to recalculate metrics
+           batch_size (int): size of inputs to pass through in a single batch
+
+        Returns:
+            tuple of dictionary containing metrics, list of error files,
+            and list of success files
+        """
+
         all_dice = []
         all_hd95 = []
         all_common_metrics = []
         error_files = []
 
         metrics_summary = {}
-        
+
         if recalculate:
                 all_dice, all_hd95, all_common_metrics, error_files = self._predict_and_calc_metrics(subj_selected=subj_names, mf_inference=mf_inference, batch_size=batch_size)
 
@@ -75,7 +117,7 @@ class MaskFormerEvaluation():
                 all_recall = [metric[MetricName.RECALL.value] for metric in all_common_metrics]
                 all_sensitivity = [metric[MetricName.SENSITIVITY.value] for metric in all_common_metrics]
                 all_specificity = [metric[MetricName.SPECIFICITY.value] for metric in all_common_metrics]
-                
+
                 metric_scores = {
                     MetricName.PRECISION.value: np.array(all_precision),
                     MetricName.RECALL.value: np.array(all_recall),
@@ -84,13 +126,13 @@ class MaskFormerEvaluation():
                     MetricName.DICE_SCORE.value: np.array(all_dice),
                     MetricName.HD95.value: np.array(all_hd95)
                 }
-                
+
                 metrics_summary = {
                     "scores": metric_scores,
                     "error_files": error_files,
                     "success_files": success_files
                 }
-                
+
                 # dump results to temp file
                 metrics_temp_file = data_handler.create_temp_file(metrics_file_name)
                 joblib.dump(metrics_summary, metrics_temp_file)
@@ -104,13 +146,23 @@ class MaskFormerEvaluation():
                                                             train_dir_prefix=metrics_dir_prefix)
 
             metrics_summary = joblib.load(metrics_temp_file)
-            
+
         print("Files with error:", metrics_summary["error_files"])
         print("Total Files evaluated: " + str(len(metrics_summary["success_files"])))
-        
+
         return metrics_summary["scores"], metrics_summary["error_files"], metrics_summary["success_files"]
 
     def get_mean_scores(self, metrics_dict):
+        """
+        Calculates the mean of each metric for all samples
+
+        Args:
+            metrics_dict (dict): dictionary containing metrics where key is
+            metric name and value is numpy array of scores for each sample
+
+        Returns:
+            pandas.DataFrame of metric mean values
+        """
         metric_names = [
             MetricName.DICE_SCORE.value,
             MetricName.HD95.value,
@@ -126,13 +178,34 @@ class MaskFormerEvaluation():
 
     # metric plotting functions
     def display_statistics(self, metrics_dict):
+        """
+        Constructs a summary statistics table for each metric
+
+        Args:
+            metrics_dict (dict): dictionary containing metrics where key is
+            metric name and value is numpy array of scores for each sample
+
+        Returns:
+            None
+        """
         for metric_name, metric_scores in metrics_dict.items():
             print("Summary:", metric_name)
             df = pd.DataFrame(metric_scores, columns=self.all_label_names)
             print(df.describe())
             print("\n")
-    
+
     def draw_box_plots(self, metrics_dict, set_ylim=False):
+        """
+        Plots metric statistics as a box plot for each segmentation label
+
+        Args:
+            metrics_dict (dict): dictionary containing metrics where key is
+            metric name and value is numpy array of scores for each sample
+            set_ylim (bool): flag to indicate whether to set ylim explicity to 1
+
+        Returns:
+            None
+        """
         nrows = 3
         ncols = 2
 
@@ -154,10 +227,20 @@ class MaskFormerEvaluation():
             count+=1
             ax.grid(True)
             self.mri_plt.set_axis_config(ax)
-            
+
         plt.show()
-        
+
     def build_histogram(self, metrics_dict):
+        """
+        Plots metrics statistics as a histogram colored by each segmentation label
+
+        Args:
+            metrics_dict (dict): dictionary containing metrics where key is
+            metric name and value is numpy array of scores for each sample
+
+        Returns:
+            None
+        """
         nrows = 3
         ncols = 2
 
@@ -180,15 +263,27 @@ class MaskFormerEvaluation():
             ax.set_xlabel(metric_name)
             ax.set_ylabel('count')
             ax.grid(True)
-            
+
             self.mri_plt.set_axis_config(ax)
-            
+
             count+=1
-            
+
         plt.show()
 
 
     def get_low_dice_score_images(self, subj_list, summary_metrics_dict, top_n=10):
+        """
+        Identifies samples with lowest dice scores for QC
+
+        Args:
+            subj_list (list): list of strings containing unique subject id's
+            summary_metrics_dict (dict): dictionary containing metrics where key is
+            metric name and value is numpy array of scores for each sample
+            top_n (int): how many records to return
+
+        Returns:
+            pandas.DataFrame of samples with top_n lowest Dice scores.
+        """
         flattened_array = summary_metrics_dict[MetricName.DICE_SCORE.value].flatten()
 
         # rearranges the elements such that the smallest n elements come before the nth index
@@ -220,6 +315,18 @@ class MaskFormerEvaluation():
 
 
     def get_high_hd95_images(self, subj_list, summary_metrics_dict, top_n=10):
+        """
+        Identifies samples with highest 95th percentile Hausdorff distance for QC
+
+        Args:
+            subj_list (list): list of strings containing unique subject id's
+            summary_metrics_dict (dict): dictionary containing metrics where key is
+            metric name and value is numpy array of scores for each sample
+            top_n (int): how many records to return
+
+        Returns:
+            pandas.DataFrame of samples with top_n highest 95th perc. Hausdorff distance
+        """
         flattened_array = summary_metrics_dict[MetricName.HD95.value].flatten()
 
         # rearranges the elements such that the smallest n elements come before the nth index
@@ -245,9 +352,19 @@ class MaskFormerEvaluation():
                 })
         top_n_worst_hd95_sorted = sorted(top_n_worst_hd95, key=lambda x: (-1*x['score']))
         return pd.DataFrame(top_n_worst_hd95_sorted)
-    
-            
+
+
     def _update_invalid_hd95(self, all_hd95):
+        """
+        Private class method to set default max possible hausdorff distance
+        max value is specified as 373.12866 per Brats 2021 challenge winner.
+
+        Args:
+            all_hd95 (list): list of hd95 scores as floats
+
+        Returns:
+            a list of floats. Does not mutate input.
+        """
         all_hd95_copy = all_hd95.copy()
         max_value = 0
         none_indices = []
@@ -264,13 +381,25 @@ class MaskFormerEvaluation():
         # why 373.12866 in Brats 2021 winner?
         max_value = 373.12866
         print("replacing invalid indices with fixed hausdorff", str(max_value))
-        print("indices with invalid hd95", str(none_indices))        
+        print("indices with invalid hd95", str(none_indices))
         # replace the invalid values with max hd value
         for row, col in none_indices:
             all_hd95_copy[row][col] = max_value
         return all_hd95_copy
-    
+
     def _calc_metric_single_segment(self, args):
+        """
+        Private class method to calculate metrics for a single segment label
+
+        Args:
+            label_idx (int): slice to calculate
+            metric_func (func): a metrics functions from utils.metrics
+            mask_pred_3d (numpy.ndarray): predicted segmentation mask
+            mask_true_3d (numpy.ndarray): ground truth segmentation maks
+
+        Returns
+            float
+        """
         label_idx, metric_func, mask_pred_3d, mask_true_3d = args
         true_mask_cur = mask_true_3d[label_idx, :, :, :]
         pred_mask_cur = mask_pred_3d[label_idx, :, :, :]
@@ -279,29 +408,64 @@ class MaskFormerEvaluation():
 
     # use pool from hausdorff distance
     def _calc_metric_all_segments_pool(self, label_names, metric_func, mask_pred_3d, mask_true_3d):
+        """
+        Private class method to parallelize computation of hausdorff distance
+
+        Args:
+            label_names (list): list of ints representing segmentation mask label names
+            metric_func (func): a metrics function from utils.metrics
+            mask_pred_3d (numpy.ndarray): predicted segmentation mask
+            mask_true_3d (numpy.ndarray): ground truth segmentation mask
+
+        Returns:
+            list of floats
+        """
         # Create a pool of worker processes
-        pool = multiprocessing.Pool()  
+        pool = multiprocessing.Pool()
         args_list = [(label_idx, metric_func, mask_pred_3d, mask_true_3d) for label_idx in range(len(label_names))]
         results = pool.map(self._calc_metric_single_segment, args_list)
-        
+
         # Close the pool to free resources
         pool.close()
-        
+
         # Wait for all processes to complete
-        pool.join()  
+        pool.join()
 
         return results
 
     def _calc_metric_all_segments(self, label_names, metric_func, mask_pred_3d, mask_true_3d):
+        """
+        Private class method to compute metrics for all segmentation labels
+
+        Args:
+            label_names (list): list of ints representing segmentation maksk label names
+            metric_func (func): a metrics function from utils.metrics
+            mask_pred_3d (numpy.ndarray): predicted segmentation mask
+            mask_true_3d (numpy.ndarray): ground truth segmentation mask
+
+        Returns:
+            list of floats
+        """
         results = []
         for label_idx in range(len(label_names)):
             true_mask_cur = mask_true_3d[label_idx,:,:,:]
             pred_mask_cur = mask_pred_3d[label_idx,:,:,:]
             result = metric_func(pred_mask_cur, true_mask_cur)
             results.append(result)
-        return results                 
+        return results
 
     def _predict_and_calc_metrics(self, subj_selected: List[str], mf_inference: MaskFormerInference, batch_size: int):
+        """
+        Private class method to generate predicted masks and calculate metrics
+
+        Args:
+            subj_selected (list): list of strings of selected subjects
+            mf_inference (MaskFormerInference): instance of MaskFormerInference class (train/val/test)
+            batch_size (int): batch size to use in processing through predict and calc
+
+        Returns:
+            tuple of lists
+        """
         all_dice = []
         all_hd95 = []
         all_common_metrics = []
@@ -313,20 +477,20 @@ class MaskFormerEvaluation():
             # perform inference for each slice within the volume
             mask_3d_pred_results = mf_inference.predict_patient_mask(subj_id=subj_id, batch_size=batch_size)
             mask_true_3d, mask_pred_3d = mask_3d_pred_results[1], mask_3d_pred_results[2]
-            
+
             # since prediction range is from 0 to 255, convert data back to binary
             mask_pred_3d_binary = mf_utils.descale_mask(mask_pred_3d)
             mask_true_3d_binary = mf_utils.descale_mask(mask_true_3d)
-            
+
             if self.use_brats_region:
                 mask_pred_3d_binary = mf_utils.to_brats_mask(mask_pred_3d)
                 mask_true_3d_binary = mf_utils.to_brats_mask(mask_true_3d)
-            
+
             try:
-                dice_score, hausdorff_val, common_metrics_dict = self.calc_metrics(subj_id=subj_id, 
+                dice_score, hausdorff_val, common_metrics_dict = self.calc_metrics(subj_id=subj_id,
                                                                                    mask_pred_binary=mask_pred_3d_binary,
                                                                                    mask_true_binary=mask_true_3d_binary)
-                
+
                 # append only when there is no error
                 all_dice.append(dice_score)
                 all_hd95.append(hausdorff_val)
@@ -337,5 +501,5 @@ class MaskFormerEvaluation():
                 error_files.append(subj_id)
 
         return  all_dice, all_hd95, all_common_metrics, error_files
-    
-    
+
+
