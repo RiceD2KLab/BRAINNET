ef0131f968c8ec305d186f5ef3236cca1cf57b1c
Ben Dowdell <ben.dowdell87@gmail.com>
2023-08-09 11:08:13 -0500

    adds docstrings to functions missing docstring

--- src/utils/maskformer_utils.py
+++ src/utils/maskformer_utils.py
@@ -15,6 +15,16 @@ ADE_MEAN = np.array([123.675, 116.280, 103.530]) / 255
 ADE_STD = np.array([58.395, 57.120, 57.375]) / 255
 
 def scale_mask(mask):
+    """
+    binarizes an input mask and then scales values to range between
+    0 and 255
+
+    Args:
+        mask (torch.Tensor or numpy.ndarray): the input mask to scale
+
+    Returns:
+        scaled torch.Tensor or numpy.ndarray
+    """
     if isinstance(mask, torch.Tensor):
         visual_mask = (mask.bool().numpy() * 255).astype(np.uint8)
     else:
@@ -23,23 +33,69 @@ def scale_mask(mask):
     # return Image.fromarray(visual_mask)
 
 def descale_mask(mask):
+    """
+    Normalizes mask
+
+    Args:
+        mask (torch.Tensor or numpy.ndarray): input mask
+
+    Returns:
+        descaled torch.Tensor or numpy.ndarray
+    """
     return (mask/255).astype(np.uint8)
 
 def softmax(logits):
+    """
+    Defines 3D softmax activation function
+
+    Args:
+        logits (torch.Tensor): final layer output to prediction layer
+
+    Returns:
+        torch.Tensor
+    """
     e_x = np.exp(logits - np.max(logits))
     return e_x / e_x.sum(axis=0)
 
 def softmax_2d(logits):
+    """
+    Defines 2D softmax activation function
+
+    Args:
+        logits (torch.Tensor): final layer output to prediction layer
+
+    Returns:
+        torch.tensor
+    """
     e_x = np.exp(logits - np.max(logits, axis=(0, 1), keepdims=True))
     return e_x / np.sum(e_x, axis=(0, 1), keepdims=True)
 
 def denormalize_img(img_data):
+    """
+    Restores image amplitude values to original scale
+
+    Args:
+        img_data (torch.Tensor or numpy.ndarray): input image normalized using ADE mean/stdev
+
+    Returns:
+        torch.Tensor or numpy.ndarray
+    """
     denormalized_image = ( img_data * np.array(ADE_STD)[:, None, None] ) + np.array(ADE_MEAN)[:, None, None]
     denormalized_image = ( denormalized_image * 255 ).astype(np.uint8)
     denormalized_image = np.moveaxis(denormalized_image, 0, -1)
     return denormalized_image
 
 def to_brats_mask(mask_3d):
+    """
+    Converts mask labels from [NCR, ET, ED, ELSE] to
+    [TC, WT, ET] for comparison with BRATS challenge reported metrics
+
+    Args:
+        mask_3d (torch.Tensor or numpy.ndarray): input mask with segmentation labels as ints
+
+    Returns:
+        torch.Tensor or numpy.ndarray
+    """
     # assumed shape:  (4, 146, 512, 512)
     # init brats mask
     brats_mask_shape = list(mask_3d.shape)
@@ -56,12 +112,32 @@ def to_brats_mask(mask_3d):
     return brats_mask
 
 def mask_to_segmentation(mask_labels, class_labels):
+    """
+    Flattens mask labels with shape (n_label, width, height) to a
+    segmentation image with shape (width, height)
+
+    Inputs:
+        mask_labels (torch.Tensor or numpy.ndarray): mask labels
+        class_labels (numpy.ndarray): class labels as ints
+
+    Returns:
+        torch.Tensor or numpy.ndarray
+    """
     # map (num_labels, width, height) to segmentation (width, height)
     input_mask_mapping = {mask_idx: label_id for mask_idx, label_id in enumerate(class_labels)}
     input_mask_to_segm = np.vectorize(input_mask_mapping.get)(mask_labels.argmax(axis=0))
     return input_mask_to_segm.astype(np.uint8)
 
 def post_proc_result_to_segmentation(results):
+    """
+    Map results['segmentation] to correct labels
+
+    Args:
+        results (torch.Tensor): predicted segmentation masks
+
+    Returns:
+        torch.Tensor
+    """
     # map results['segmentation'] to correct labels
     # note that id represents obj instances and can be different from label id
     # [{'id': 0, 'label_id': 0, 'was_fused': False, 'score': 0.99242},
@@ -76,6 +152,16 @@ def post_proc_result_to_segmentation(results):
     return pred_mask_to_segm
 
 def get_subj_ids(subj_files):
+    """
+    Compiles a list of unique subject id numbers
+    Ex: UPENN-GBM-00001 --> 00001
+
+    Args:
+        subj_files (list): list of strings of unique subjects
+
+    Returns:
+        list of strings
+    """
     # get unique subj train ids: [0001, 0002, 0003 ..]
     subj_ids = []
     for subj in subj_files:
@@ -85,6 +171,20 @@ def get_subj_ids(subj_files):
     return subj_ids
 
 def get_subset_files(subj_files, file_no_min, file_no_max, subj_id_min, subj_id_max):
+    """
+    Finds all associated MRI and mask volumes for a given subject unique id within
+    a specified range of files
+
+    Args:
+        subj_files (DataHandler): DataHandler instance (train/val/test)
+        file_no_min (int): Minimum 2D slice number
+        file_no_max (int): Maximum 2D slice number
+        subj_id_min (int): Minimum subject index in subject list
+        subj_id_max(int): Maximum subject index in subject list
+
+    Returns:
+        list of strings, sorted ascending
+    """
     subj_ids = get_subj_ids(subj_files)
     # filter files within range
     subj_filenames = []
@@ -107,6 +207,19 @@ def get_subset_files(subj_files, file_no_min, file_no_max, subj_id_min, subj_id_
     return subj_sorted
 
 def get_all_subj_ids(data_dir):
+    """
+    Compiles a list of all unique subject ids
+
+    Ex: files: UPENN-GBM-00006_11_FLAIR_1.nii.gz, UPENN-GBM-00006_11_T1_1.nii.gz ...
+    --> unique subj_id: UPENN-GBM-00006
+    --> all subject ids [UPENN-GBM-00006, UPENN-GBM-00008, UPENN-GBM-00013, ...]
+
+    Args:
+        data_dir (str): path to data directory
+
+    Returns:
+        list of strings, sorted ascending
+    """
     # Obtain full  dataset
     # UPENN-GBM-00006_11_FLAIR_1.nii.gz, UPENN-GBM-00006_11_T1_1.nii.gz, UPENN-GBM-00006_11_FLAIR_2.nii.gz...
     all_slices = os.listdir(data_dir)
@@ -117,6 +230,20 @@ def get_all_subj_ids(data_dir):
     return all_subjs
 
 def plot_mask_labels(class_labels, pixel_values, mask_labels, title, scale=True):
+    """
+    Creates a figure visualizing an input image slice and its corresponding
+    class label masks.
+
+    Args:
+        class_labels (list): class labels
+        pixel_values (numpy.ndarray): 2D MRI image
+        mask_labels (array-like): 2D mask array
+        title (str): figure title
+        scale (bool): flag indicating whether to binarize and re-scale mask [0, 255]
+
+    Returns:
+        None.
+    """
     n_image = len(class_labels) + 1
     # plot input pixel values
     fig, axs = plt.subplots(nrows=1, ncols=n_image, figsize=(5*n_image, 5))
@@ -134,6 +261,23 @@ def plot_mask_labels(class_labels, pixel_values, mask_labels, title, scale=True)
     plt.show()
 
 def plot_mask_comparison(input_class_labels, pred_class_labels, input_pixel_values, input_mask_labels, pred_mask_labels, title, scale=True):
+    """
+    Plots predicted segment over input of segment
+    note: if a segment is predicted but does not exist in the input mask,
+    it will not be visualized
+
+    Args:
+        input_class_labels (numpy array-like): ground truth labels
+        pred_class_labels (numpy array-like): predicted labels
+        input_pixel_values (numpy array-like): 2D input image
+        input_mask_labels (numpy array-like): 2D ground truth mask
+        pred_mask_labels (numpy array-like): 2D predicted mask
+        title (str): figure title
+        scale (bool): flag indicating whether to binarize mask and scale [0, 255]
+
+    Returns:
+        None.
+    """
 
     n_image = len(input_class_labels) + 1
     fig, axs = plt.subplots(nrows=1, ncols=n_image, figsize=(5*n_image, 5))
@@ -158,11 +302,24 @@ def plot_mask_comparison(input_class_labels, pred_class_labels, input_pixel_valu
                            legends=["input", "predicted"],
                            title=segment_name,
                            fig=fig, axs=axs, row=0, col=axs_idx)
-        
+
     plt.tight_layout()
     plt.show()
 
 def plot_segmentation_comparison(input_pixel_values, input_segmentation, pred_segmentation, title, loc="lower right"):
+    """
+    Plots ground truth and predicted segmentation masks as single image instead of separately.
+
+    Args:
+        input_pixel_values (array-like): 2D MRI image
+        input_segmentation (array-like): 2D ground truth segmentation mask
+        pred_segmentation (array-like): 2D predicted segmentation mask
+        title (str): Figure title
+        loc (str): location for placing axes legend ["lower right", "upper right", "lower left", "upper left"]
+
+    Returns:
+        None
+    """
     n_image = 3
     fig, axs = plt.subplots(nrows=1, ncols=n_image, figsize=(4*n_image, 4))
     denormalized_img = denormalize_img(input_pixel_values)
@@ -174,6 +331,16 @@ def plot_segmentation_comparison(input_pixel_values, input_segmentation, pred_se
     plt.show()
 
 def resize_mask(mask, original_size):
+    """
+    Rescale mask to original dimensions
+
+    Args:
+        mask (array-like): segmentation mask
+        original_size (tuple): tuple of ints representing original image dimensions
+
+    Returns:
+        array-like
+    """
     # Note: cv2.resize expects the size in (width, height) format
     original_size = (original_size[1], original_size[0])  # Switching from HxW to WxH
 
@@ -197,6 +364,16 @@ def resize_mask(mask, original_size):
     return resized_mask
 
 def _extract_numeric_part(filename):
+    """
+    Helper function for extracting slice number for sorting
+    Ex:
+    input filename: 'UPENN-GBM-00131_50.nii.gz'
+    prefix: 'UPENN-GBM-00131'
+    numeric_part: 50
+
+    Args:
+        filename (str): a valid file name with a unique subj id and a slice number
+    """
     # Expected filename: "UPENN-GBM-00131_50.nii.gz"
     # Split the filename based on underscore
 
